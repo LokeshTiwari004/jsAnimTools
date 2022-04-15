01c967da597f474469e331813037c2489d136f54
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-04-15 22:08:05 +0530

    De-clustered TimingFunction.js

---
+++ TimingFunctions/linear.js
@@ -0,0 +1,8 @@
+export default function () {
+  return {
+    name: 'linear',
+    func: function (timeFraction) {
+      return timeFraction;
+    }
+  }
+}
---
+++ TimingFunctions/timingFunctions.js
@@ -0,0 +1,12 @@
+import getNthPol from './getNthPol.js';
+import linear from './linear.js';
+import pseudoBounce from './pseudoBounce.js';
+import quad from './quad.js';
+
+
+export default {
+    linear,
+    quad,
+    getNthPol,
+    pseudoBounce,
+}
